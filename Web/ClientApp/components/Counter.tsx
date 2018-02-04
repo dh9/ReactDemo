c7f0815e7cea61949c211627683a3231cb857e50
dh9 <dh9@live.co.uk>
2018-02-04 21:17:40 +0000

    added small change to Counter

--- Web/ClientApp/components/Counter.tsx
+++ Web/ClientApp/components/Counter.tsx
@@ -15,7 +15,7 @@ class Counter extends React.Component<CounterProps, {}> {
         return <div>
             <h1>Counter</h1>
 
-            <p>This is a simple example of a React component.</p>
+            <p>This is a very simple example of a React component.</p>
 
             <p>Current count: <strong>{ this.props.count }</strong></p>
 
